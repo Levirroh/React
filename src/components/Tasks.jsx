fc5e034e3fe762d670fb4ae39b2a2055c718d7e1
Levirroh <johann.gr14@gmail.com>
2025-02-04 21:56:02 -0300

    feat: depending on the status of the task it changes its design

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -7,10 +7,9 @@ function Tasks(props){
                 <li key={task.id} className="flex gap-2">
                 <button
                  onClick={() => props.onTaskClick(task.id)} 
-                 className="bg-slate-400 text-left text-white p-2 rounded-md w-full"
-                 >
+                 className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted && "line-through"}`}
+                 > 
                     {task.title}
-                    {task.isCompleted ? "COMPLETE" : "INCOMPLETE"}
                 </button> 
                 <button className="bg-slate-400 p-2 rounded-md text-white">
                     <ChevronRightIcon/>
